bbfd7e04fe2a0cca4d780821bbf340d0241a2b3d
Florian Klemme <mail@florianklemme.de>
2018-01-18 14:08:38 +0100

    Removed some whitespace.

--- src/ast_types_adapted.hpp
+++ src/ast_types_adapted.hpp
@@ -7,35 +7,35 @@
 
 // Value types
 BOOST_FUSION_ADAPT_STRUCT(
-    liberty::ast::unit_t,
-    (double, number)
-    (std::string, unit))
+  liberty::ast::unit_t,
+  (double, number)
+  (std::string, unit))
 
 // Workaround: No adapt for single member structs
 // https://stackoverflow.com/a/19824426/9224499
 //BOOST_FUSION_ADAPT_STRUCT(
-//    liberty::ast::word_t,
-//    (std::string, string))
+//  liberty::ast::word_t,
+//  (std::string, string))
 
 //BOOST_FUSION_ADAPT_STRUCT(
-//    liberty::ast::quoted_t,
-//    (std::string, string))
+//  liberty::ast::quoted_t,
+//  (std::string, string))
 
 // Element types
 BOOST_FUSION_ADAPT_STRUCT(
-    liberty::ast::container_t,
-    (std::string, name)
-    (std::vector<std::string>, args)
-    (std::vector<liberty::ast::element_t>, elements))
+  liberty::ast::container_t,
+  (std::string, name)
+  (std::vector<std::string>, args)
+  (std::vector<liberty::ast::element_t>, elements))
 
 BOOST_FUSION_ADAPT_STRUCT(
-    liberty::ast::list_t,
-    (std::string, name)
-    (std::vector<liberty::ast::value_t>, values))
+  liberty::ast::list_t,
+  (std::string, name)
+  (std::vector<liberty::ast::value_t>, values))
 
 BOOST_FUSION_ADAPT_STRUCT(
-    liberty::ast::pair_t,
-    (std::string, name)
-    (liberty::ast::value_t, value))
-
+  liberty::ast::pair_t,
+  (std::string, name)
+  (liberty::ast::value_t, value))
+  
 // clang-format on
